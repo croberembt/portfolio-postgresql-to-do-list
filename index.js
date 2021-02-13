6a1311ae3725d7d925e3cc2cec7605b8215f740a
Calli Oberembt <croberembt@gmail.com>
2021-02-12 18:54:56 -0600

    getting ready for deployment

--- index.js
+++ index.js
@@ -2,10 +2,23 @@ const express = require('express');
 const app = express();
 const cors = require('cors'); 
 const pool = require('./db'); 
+const path = require('path'); 
+const PORT = process.env.PORT || 5000; 
+
+//process.env.PORT
+//process.env.NODE_ENV => production or undefined
+
+
+
 
 // middleware
+
 app.use(cors()); 
 app.use(express.json()); 
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')))
+}
 
 // ROUTES
 
@@ -84,6 +97,6 @@ app.delete('/todolist/:id', async (req, res) => {
   }
 })
 
-app.listen(5000, () => {
-  console.log('server is listening on port 5000');
+app.listen(PORT, () => {
+  console.log(`server is listening on port ${PORT}`);
 }); 
